6ea01a242efdf44ec92c318e70fd228f5372d69f
jax <169969144+tooltop@users.noreply.github.com>
2024-06-18 01:18:03 +0800

    Update index.js
    Added addressee number

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-CHAT_ID = globalThis.TG_CHAT_ID
-TOKEN = globalThis.TG_TOKEN
-
-class Handler {
-
-  async send(req) {
-    const formData = await req.formData()
-    const body = {}
-    for (const entry of formData.entries()) {
-      body[entry[0]] = entry[1]
+(() => {
+  // index.js
+  CHAT_ID = globalThis.TG_CHAT_ID;
+  TOKEN = globalThis.TG_TOKEN;
+  var Handler = class {
+    async send(req) {
+      const formData = await req.formData();
+      const body = {};
+      for (const entry of formData.entries()) {
+        body[entry[0]] = entry[1];
+      }
+      const msg = {
+        chat_id: CHAT_ID,
+        text: "From: " + body["From"] + "\r\nT   o  : " + body["To"] + "\r\n==== Message: ====\r\n" + body["Body"]
+      };
+      const init = {
+        body: JSON.stringify(msg),
+        method: "POST",
+        headers: {
+          "content-type": "application/json;charset=UTF-8"
+        }
+      };
+      let url = "https://api.telegram.org/bot" + TOKEN + "/sendMessage";
+      console.log(await fetch(url, init));
+      return new Response("", { status: 200 });
     }
-
-    const msg = {
-      chat_id: CHAT_ID,
-      text: "From:" + body['From'] + "\r\n==== Message: ====\r\n" + body['Body'],
-    }
-    const init = {
-      body: JSON.stringify(msg),
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json;charset=UTF-8',
-      },
-    }
-
-    let url = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage'
-    console.log(await fetch(url, init))
-    return new Response('', {status: 200})
-  }
-}
-
-// Initialize new request handler
-const handler = new Handler()
-
-// Listen to all fetch events received by worker
-addEventListener('fetch', event => {
-  event.respondWith(handler.send(event.request))
-})
+  };
+  var handler = new Handler();
+  addEventListener("fetch", (event) => {
+    event.respondWith(handler.send(event.request));
+  });
+})();
+//# sourceMappingURL=index.js.map
